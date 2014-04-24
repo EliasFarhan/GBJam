991afa2c35eca4e3ae187bceeeb506553d0870a5
Elias Farhan <elias.farhan@gmail.com>
2014-04-24 10:18:54 +0200

    Begininng network protocol

--- network/client.py
+++ network/client.py
@@ -1,21 +1,16 @@
 import socket
 import sys
 
-HOST, PORT = "eliasfarhan.ch", 1234
-data = "Hello world!"
+HOST, PORT = "eliasfarhan.ch", 9999
+data = "ID_REQUEST"
 
-# Create a socket (SOCK_STREAM means a TCP socket)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# SOCK_DGRAM is the socket type to use for UDP sockets
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-try:
-    # Connect to server and send data
-    sock.connect((HOST, PORT))
-    sock.sendall(data + "\n")
-
-    # Receive data from the server and shut down
-    received = sock.recv(1024)
-finally:
-    sock.close()
+# As you can see, there is no connect() call; UDP has no connections.
+# Instead, data is directly sent to the recipient via sendto().
+sock.sendto(data, (HOST, PORT))
+received = sock.recv(1024)
 
 print "Sent:     {}".format(data)
 print "Received: {}".format(received)
--- network/server.py
+++ network/server.py
@@ -1,29 +1,28 @@
 import SocketServer
 
+client_id = 0
 
-class MyTCPHandler(SocketServer.BaseRequestHandler):
+class MyUDPHandler(SocketServer.BaseRequestHandler):
     """
-    The RequestHandler class for our server.
-
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
+    This class works similar to the TCP handler class, except that
+    self.request consists of a pair of data and client socket, and since
+    there is no connection the client address must be given explicitly
+    when sending data back via sendto().
     """
 
     def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
+        global client_id
+        data = self.request[0].strip()
+        socket = self.request[1]
         print "{} wrote:".format(self.client_address[0])
-        print self.data.decode('utf-8')
-        # just send back the same data, but upper-cased
-        self.request.sendall("You are :"+str(self.client_address[0])+" and you said: "+self.data)
+        print data
 
+        if data == 'ID_REQUEST':
+            client_id += 1
+            socket.sendto("NEW_ID;"+client_id, self.client_address)
+        else:
+            socket.sendto("ERROR;BAD_REQUEST", self.client_address)
 if __name__ == "__main__":
-    HOST, PORT = "92.222.15.13", 1234
-
-    # Create the server, binding to localhost on port 9999
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
-
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
+    HOST, PORT = "92.222.15.13", 9999
+    server = SocketServer.UDPServer((HOST, PORT), MyUDPHandler)
     server.serve_forever()
